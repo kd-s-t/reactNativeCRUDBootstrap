6ad6132c5fbf40fa1c6a5d84445a78d1c5c1eb1e
Kenn Tinio <kendantinio@gmail.com>
2019-03-29 03:07:23 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# React Native - Note Application with Bootstrap (CRUD)  
+Development time version 0.59  
+
+## UI  
+### View 1 - Empty 
+<img src="assets/img/note1.jpg" width="30%">  
+
+### View 2 - Populated
+<img src="assets/img/note2.jpg" width="30%"> 
+
+Documentation for Styling
+[React Native Elements](https://react-native-training.github.io/react-native-elements/docs/getting_started.html)  
+
---
+++ components/Main.js
@@ -0,0 +1,184 @@
+import React from 'react';
+import { StyleSheet, Text, View, ScrollView, TextInput } from 'react-native';
+import { Button, ListItem, Header, Overlay, Input, Card, Divider } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const list = [
+  {
+    name: 'Amy Farha',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    subtitle: 'Vice President'
+  },
+  {
+    name: 'Amy Farha',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    subtitle: 'Vice President'
+  },
+  {
+    name: 'Amy Farha',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    subtitle: 'Vice President'
+  },
+  {
+    name: 'Amy Farha',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    subtitle: 'Vice President'
+  },
+  {
+    name: 'Chris Jackson',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+    subtitle: 'Vice Chairman'
+  },
+  {
+    name: 'Chris Jackson',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+    subtitle: 'Vice Chairman'
+  },
+  {
+    name: 'Chris Jackson',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+    subtitle: 'Vice Chairman'
+  },
+  {
+    name: 'Chris Jackson',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+    subtitle: 'Vice Chairman'
+  },
+  {
+    name: 'Chris Jackson',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+    subtitle: 'Vice Chairman'
+  },
+  {
+    name: 'Chris Jackson',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+    subtitle: 'Vice Chairman'
+  },
+  {
+    name: 'Chris Jackson',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+    subtitle: 'Vice Chairman'
+  },
+  {
+    name: 'Chris Jackson',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+    subtitle: 'Vice Chairman'
+  },
+]
+
+export default class Main extends React.Component {
+
+	constructor(props){
+		super(props);
+		this.state = {
+		  isVisible: false,
+		  closeLoading: false,
+		  addLoading: false,
+	      noteArray: [],
+	      noteText: ''
+		}
+	}
+
+	render() {
+	    return (
+			<View>
+				<Header>
+					<Text></Text>
+					<Text h1>My Notes</Text>
+					<Button
+					  icon={{
+					    name: "plus",
+					    size: 20,
+					    color: "white",
+					    type: "font-awesome"
+					  }}
+					  onPress={this.showAddModal.bind(this)}
+					/>
+				</Header>
+
+				<ScrollView>
+					<View>
+					  {
+					    this.state.noteArray.map((l, i) => (
+					      <ListItem
+					        key={i}
+					        title={l.name}
+					        subtitle={l.date}
+        					rightIcon={<Icon name='trash' type='font-awesome' color="red" onPress={()=> this.deleteNote(i)}  />}
+					      />
+					    ))
+					  }
+					</View>
+				</ScrollView>
+
+				<Overlay
+					isVisible={this.state.isVisible}
+					width="80%"
+					height="auto"
+				>
+					<View>
+						<Text style={{marginBottom: 10}}>
+							Add new note
+						</Text>
+						<Divider style={{ backgroundColor: 'white', height: 4 }} />
+						<Input 
+							placeholder="Something you must not forget..."
+				            onChangeText={(noteText) => this.setState({noteText})}
+				            value={this.state.noteText}
+				        />
+						<Divider style={{ backgroundColor: 'white', height: 4 }} />
+						<Button
+							loading={this.state.addLoading}
+							backgroundColor='#03A9F4'
+							buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+							title='Add now' 
+							onPress={this.addModal.bind(this)} 
+						/>
+						<Button
+							loading={this.state.closeLoading}
+							type="outline"
+							backgroundColor='lightred'
+							buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+							title='Close' 
+							onPress={this.closeAddModal.bind(this)} 
+						/>
+					</View>
+				</Overlay>
+			</View>
+	    );
+	}
+
+	showAddModal(){
+      	this.setState({ isVisible: true });
+      	this.setState({ closeLoading: false });
+	}
+
+	addModal(){
+      	this.setState({ addLoading: true });
+	    if(this.state.noteText){
+
+	      var d = new Date();
+	      this.state.noteArray.push({
+			'date': d.getFullYear()+ "/" + (d.getMonth() + 1) + "/" + d.getDate(),
+			'name': this.state.noteText
+	      });
+
+	      this.setState({ noteArray: this.state.noteArray });
+	      this.setState({ noteText: '' });
+
+	    }
+
+  		this.setState({ addLoading: false });
+      	this.setState({ isVisible: false });
+	}
+
+	closeAddModal(){
+      	this.setState({ isVisible: false });
+      	this.setState({ closeLoading: true });
+	}
+
+	deleteNote(key){
+	    this.state.noteArray.splice(key, 1);
+	    this.setState({noteArray: this.state.noteArray})
+	}
+}
